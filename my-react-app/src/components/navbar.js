fac9d10eca448f9c702376f23881b0f78ae6f186
Ion Lund <ion.lund.sthlm22@amerikanskagymnasiet.se>
2024-11-26 23:42:58 +0100

    Update navbar.js

--- my-react-app/src/components/navbar.js
+++ my-react-app/src/components/navbar.js
@@ -32,23 +32,6 @@ function Navbar() {
     <AppBar position="static">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-        <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/page1"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
           {/* Mobile menu icon */}
           <Box sx={{flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
@@ -87,24 +70,6 @@ function Navbar() {
             </Menu>
           </Box>
           {/* Desktop links */}
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/page1"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
