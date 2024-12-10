bb94542e972ef591d252a18c923f8c7372b072b8
Ion Lund <ion.lund.sthlm22@amerikanskagymnasiet.se>
2024-12-10 18:14:25 +0100

    fixing item not found

--- my-react-app/src/components/Footer.js
+++ my-react-app/src/components/Footer.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import "./footer.css";
-import Item from "Item";
+import Item from "./Item";
 
 
 const Footer = () => {
