2bb4ddc3feb5fbfb92b316f700acb57887e5d793
Ion Lund <ion.lund.sthlm22@amerikanskagymnasiet.se>
2024-12-10 18:17:56 +0100

    Update page1.js

--- my-react-app/src/pages/page1.js
+++ my-react-app/src/pages/page1.js
@@ -2,8 +2,17 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid2';
 import "./home.css"
-import Item from "../components/Item"
 import Bild1 from "./Bild1.jpeg";
+
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: "transparent",
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: "black",
+}));
 
 const Home = () => {
     return (
@@ -13,30 +22,21 @@ const Home = () => {
                     {xs: 12, md: 6}
                     }>
                     <Grid id="grid1" container spacing={2}>
-                        <Grid item size={{xs: 12, md: 6}}>
-                            <Item>
-                        <h1>Information Box</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
-                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
-                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
-                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
-                                Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
-                                Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
-                                Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
-                            </p>
-                            </Item>
-                        </Grid>
-                        <Grid item size={{xs: 12, md: 6}}>
+                        <Grid item size={12}>
                         <Item>
                         <h1>Information Box</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
                                 Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
                                 Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
-                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem.
                                 Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
                                 Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
                                 Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
-                            </p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                </p>
                             </Item>
                             </Grid>
                             <Grid item size={12}>
@@ -45,11 +45,15 @@ const Home = () => {
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
                                 Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
                                 Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
-                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem.
                                 Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus. 
                                 Maecenas ornare tortor. Donec sed tellus eget sapien fringilla nonummy. Mauris a ante. Suspendisse quam sem, consequat at, commodo vitae, feugiat in, nunc. Morbi imperdiet augue quis tellus. 
                                 Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue.
-                            </p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. 
+                                Morbi vel arcu. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. 
+                                Praesent et diam eget libero egestas mattis. Sed convallis augue vitae lorem.
+                                Nullam euismod convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. 
+                                </p>
                             </Item>
                             </Grid>
                     </Grid>
